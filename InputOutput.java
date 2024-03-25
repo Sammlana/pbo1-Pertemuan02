e9156ce43b43becf733f5dbaf943eae21ad32f3c
Sammlana <jokolkkpk@gmail.com>
2024-03-25 14:13:20 +0800

    up1

--- InputOutput.java
+++ InputOutput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public Class InputOutput{
+public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
